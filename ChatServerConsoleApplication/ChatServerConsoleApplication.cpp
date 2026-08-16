176f223173e529b19aabfc76d466127a67a0a500
Spageetus <95888955+Spageetus@users.noreply.github.com>
2026-08-16 14:30:56 -0400

    fixed bug where the server would only send 8 bytes to the client

--- ChatServerConsoleApplication/ChatServerConsoleApplication.cpp
+++ ChatServerConsoleApplication/ChatServerConsoleApplication.cpp
@@ -34,26 +34,27 @@ int main()
 
     std::cout << "Server accepted incoming connections!" << std::endl;
 
-    //read a message
-    std::cout << "Waiting for a message from the client..." << std::endl;
-    result = server.readMessage(readBuffer);
-    if (result != StatusCode::SUCCESS)
+    while (server.isActive())
     {
-        std::cout << "Server read failed with code: " << (int)result << std::endl;
-        return (int)result;
+        //read a message
+        std::cout << "Waiting for a message from the client..." << std::endl;
+        result = server.readMessage(readBuffer);
+        if (result != StatusCode::SUCCESS)
+        {
+            std::cout << "Server read failed with code: " << (int)result << std::endl;
+            return (int)result;
+        }
+        std::cout << "[Recieved Message] " << readBuffer << std::endl;
+
+        std::cout << "Send a message to the client" << std::endl;
+        //send a message
+        char* message = new char[256];
+        std::cin.getline(message, 256);
+        result = server.sendMessage(message, std::strlen(message));
+        if (result != StatusCode::SUCCESS) return (int)result;
+        std::cout << "[Sent] " << message << std::endl;
+
     }
-    std::cout << "[Recieved Message] " << readBuffer << std::endl;
-
-    std::cout << "Send a message to the client" << std::endl;
-    //send a message
-    char* message = new char[256];
-    std::cin.getline(message, 256);
-    result = server.sendMessage(message, sizeof(message));
-    if (result != StatusCode::SUCCESS) return (int)result;
-    std::cout << "[Sent] " << message << std::endl;
-
-
-
     server.stop();
 }
 
